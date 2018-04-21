dc8af6303c98cd66e0802177eefa59d4f3ccee99
Eugene Davis <eugene@industrialdragonfly.com>
2018-04-21 15:57:31 +0200

    Moved convert-shell into svg-converter folder

---
+++ convert-shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Used to get a shell into an svg-converter container and execute a basic command
+# Will be expanded later
+# Example:
+# bash .automation/convert-shell.sh svgexport bits-must-flow
+
+command=$1
+name=$2
+
+
+docker run -it --rm -v $(pwd)/$name/fonts/:/root/.local/share/fonts/TTF/type1 -v $(pwd)/$name:/convert -v $(pwd)/../watermark:/watermark svg-converter /bin/bash -c "cd /convert && ${command} ${name} ${3}"
